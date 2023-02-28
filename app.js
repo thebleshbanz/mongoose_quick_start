0b9254f0a1da189a32b93e9a509138b1cac965ba
ashishv-questglt <ashishbanjare@questglt.org>
2023-02-28 18:35:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// get quick start
+const mongoose = require();
+
+main().catch(err => console.log(err));
+
+async function  () {
+    await mongoose.connect('mongodb+srv://ngs-myblog:NuDrRHtK7quS3Cpn@myblog.tfrtibr.mongodb.net/admin?authSource=admin&replicaSet=atlas-pt12nv-shard-0&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true');
+}
