d7e57fc10ddd1c454d8e9d473658aeb026ae1706
thebleshbanz <bleshbanz@gmail.com>
2023-03-11 12:47:27 +0530

    quick start - blogModel  commit

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-// get quick start
-const mongoose = require();
+// getting started 
+
+const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
-async function  () {
-    await mongoose.connect('mongodb+srv://ngs-myblog:NuDrRHtK7quS3Cpn@myblog.tfrtibr.mongodb.net/admin?authSource=admin&replicaSet=atlas-pt12nv-shard-0&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true');
+async function main(){
+    await mongoose.connect('mongodb+srv://ngs-myblog:NuDrRHtK7quS3Cpn@myblog.tfrtibr.mongodb.net/?retryWrites=true');
 }
+
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function speak(){
+    const greeting = this.name ? "Meaow name is " + this.name : "I don't have name";
+    console.log(greeting);
+}
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const silence = new Kitten({ name : "Silence" });
+// console.log(silence.name);
+
+/* const fluffy = new Kitten({name: 'manjuBilli'});
+fluffy.save();
+// Meaow name is fluffy
+fluffy.speak();  */
+
+const kittens = Kitten.find({ name : /^fluff/});
+console.log(kittens);
---
+++ blog.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb+srv://ngs-myblog:NuDrRHtK7quS3Cpn@myblog.tfrtibr.mongodb.net/?retryWrites=true');
+}
+
+const  blogSchema = new Schema({
+    title: String, // String is shorthand for {type:Sring}
+    author: {
+        type:String
+    },
+    body: String,
+    comments:[{ body:String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    meta: {
+        votes: Number,
+        favs: Number
+    }
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
